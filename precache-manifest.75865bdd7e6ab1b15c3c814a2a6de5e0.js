c89e4c26a8af9941a32966b3b7815bd359aac87a
Njegos Rajcevic <njegos.rajcevic@klika.ba>
2020-02-06 20:31:36 +0100

    Updates

---
+++ precache-manifest.75865bdd7e6ab1b15c3c814a2a6de5e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5908f6357a27921d4a02ea43197e3968",
+    "url": "/RGB_Color_Game/index.html"
+  },
+  {
+    "revision": "59eb86ae7be41c5323b1",
+    "url": "/RGB_Color_Game/static/css/main.970f257a.chunk.css"
+  },
+  {
+    "revision": "f0dbd92226e23aa24660",
+    "url": "/RGB_Color_Game/static/js/2.b8db0481.chunk.js"
+  },
+  {
+    "revision": "59eb86ae7be41c5323b1",
+    "url": "/RGB_Color_Game/static/js/main.c64d519f.chunk.js"
+  },
+  {
+    "revision": "5471fc40737d43c1bf83",
+    "url": "/RGB_Color_Game/static/js/runtime-main.88c538b2.js"
+  }
+]);
